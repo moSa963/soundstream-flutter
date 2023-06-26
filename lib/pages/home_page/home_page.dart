5cfb2821f2271376a873da51e42d102dc9bb162d
M S <mo.sa963@hotmail.com>
2023-06-26 18:44:14 +0300

    Add home_page_bottom_navigator

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:soundstream_flutter/pages/home_page/home_page_bottom_navigator.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,11 +9,19 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  
+  int _index = 0;
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      bottomNavigationBar: HomePageBottomNavigator(
+        index: _index,
+        onChange: (i) => setState(() {
+          _index = i;
+        }),
+      ),
     );
   }
 }
---
+++ lib/pages/home_page/home_page_bottom_navigator.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class HomePageBottomNavigator extends StatelessWidget {
+  final int index;
+  final void Function(int index) onChange;
+
+  const HomePageBottomNavigator({super.key, required this.index, required this.onChange});
+
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return BottomNavigationBar(
+      currentIndex: index,
+      onTap: onChange,
+      selectedItemColor: Theme.of(context).primaryColor,
+      unselectedItemColor: Theme.of(context).primaryColor,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: "Home",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search),
+          label: "Search",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.library_books),
+          label: "Library",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.account_box),
+          label: "Account",
+        ),
+      ],
+    );
+  }
+}
