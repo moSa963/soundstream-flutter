0f978ddc0fb45e6c324b51de8ee70999afdf07d7
M S <mo.sa963@hotmail.com>
2023-06-26 21:10:46 +0300

    Add Track model

---
+++ lib/models/track.dart
@@ -0,0 +1,33 @@
+import 'package:soundstream_flutter/models/playlist.dart';
+import 'package:soundstream_flutter/models/user.dart';
+
+
+
+class Track{
+  int id;
+  User? user;
+  String title;
+  String writtenBy;
+  String performedBy;
+  Playlist? album;
+  DateTime? createdAt;
+  DateTime? addedAt;
+  bool liked;
+  int duration;
+  bool explicit;
+
+  Track({
+    required this.id,
+    this.title="",
+    this.writtenBy="",
+    this.performedBy="",
+    this.album,
+    this.liked=false,
+    this.explicit=true,
+    this.duration=0,
+    this.user,
+    this.createdAt,
+    this.addedAt,
+  });
+
+}
