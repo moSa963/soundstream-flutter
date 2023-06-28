099cf990a05c650d0825f9c923918307c58b827f
M S <mo.sa963@hotmail.com>
2023-06-28 11:06:42 +0300

    Add likedTracks list item to library page

--- lib/pages/library_page/library_page.dart
+++ lib/pages/library_page/library_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:soundstream_flutter/providers/playlists_provider.dart';
+import 'package:soundstream_flutter/widgets/list_item.dart';
 import 'package:soundstream_flutter/widgets/playlist_item.dart';
-
 
 class LibraryPage extends StatefulWidget {
   const LibraryPage({super.key});
@@ -11,18 +11,30 @@ class LibraryPage extends StatefulWidget {
   State<LibraryPage> createState() => _LibraryPageState();
 }
 
-
 class _LibraryPageState extends State<LibraryPage> {
-
-
   @override
   Widget build(BuildContext context) {
     var playlists = context.watch<PlaylistsProvider>().playlists;
 
     return ListView(
       children: [
-        for(var playlist in playlists) PlaylistItem(playlist: playlist)
+        likedTracksItem(),
+        for (var playlist in playlists) PlaylistItem(playlist: playlist)
       ],
+    );
+  }
+
+  Widget likedTracksItem() {
+    return ListItem(
+      title: "Liked tracks",
+      subtitle: "Playlist",
+      leading: AspectRatio(
+        aspectRatio: 1,
+        child: Container(
+          color: Theme.of(context).colorScheme.primary.withAlpha(200),
+          child: const Icon(Icons.favorite),
+        ),
+      ),
     );
   }
 }
