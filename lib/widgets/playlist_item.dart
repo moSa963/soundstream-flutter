28c8fe858dab3b91bf9448bb51163b4c834357f4
M S <mo.sa963@hotmail.com>
2023-06-28 23:17:08 +0300

    Add _openShowPlaylistPage method to PlaylistItem

--- lib/widgets/playlist_item.dart
+++ lib/widgets/playlist_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:soundstream_flutter/models/playlist.dart';
+import 'package:soundstream_flutter/pages/show_playlist_page/show_playlist_page.dart';
 import 'package:soundstream_flutter/widgets/list_item.dart';
 
 class PlaylistItem extends StatelessWidget {
@@ -10,12 +11,22 @@ class PlaylistItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListItem(
+      onTap: () => _openShowPlaylistPage(context),
       leading: Image.network(
-          'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
-          fit: BoxFit.contain,
-        ),
-        title: playlist.title,
-        subtitle: "${playlist.album ? "Album" : "Playlist"} . ${playlist.user?.username ?? ""}",
+        'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
+        fit: BoxFit.contain,
+      ),
+      title: playlist.title,
+      subtitle:
+          "${playlist.album ? "Album" : "Playlist"} . ${playlist.user?.username ?? ""}",
     );
+  }
+
+  void _openShowPlaylistPage(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) {
+        return ShowPlaylistPage(playlist: playlist,);
+      },
+    ));
   }
 }
