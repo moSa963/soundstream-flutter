171ab54638f97f7fe5150e1918a149a66b5837f2
M S <mo.sa963@hotmail.com>
2023-06-29 22:39:40 +0300

    Add playlist service to PlaylistsProvider

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,12 @@ import 'package:provider/provider.dart';
 import 'package:soundstream_flutter/pages/home_page/home_page.dart';
 import 'package:soundstream_flutter/providers/playlists_provider.dart';
 import 'package:soundstream_flutter/providers/theme_provider.dart';
+import 'package:soundstream_flutter/services/playlist_service.dart';
 
 void main() => runApp(ChangeNotifierProvider<ThemeProvider>(
       create: (_) => ThemeProvider(),
       child: ChangeNotifierProvider<PlaylistsProvider>(
-        create: (_) => PlaylistsProvider(),
+        create: (_) => PlaylistsProvider(service: PlaylistService()),
         child: const MyApp(),
       ),
     ));
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Soundstream',
       theme: context.watch<ThemeProvider>().themeData,
-      home: const HomePage(),
+      home: HomePage(),
     );
   }
 }
--- lib/providers/playlists_provider.dart
+++ lib/providers/playlists_provider.dart
@@ -1,21 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:soundstream_flutter/models/playlist.dart';
+import 'package:soundstream_flutter/services/playlist_service.dart';
 
 class PlaylistsProvider with ChangeNotifier {
   final List<Playlist> _playlists;
   List<Playlist> get playlists => _playlists;
 
-  PlaylistsProvider() : _playlists = [];
+  bool _loading;
+  bool get loading => _loading;
+
+  final PlaylistService service;
+
+  PlaylistsProvider({required this.service})
+      : _playlists = [],
+        _loading = false {
+          loadPlaylists();
+        }
 
   void loadPlaylists() async {
-    //TODO
-    throw UnimplementedError();
+    _loading = true;
+    var res = await service.list();
+    _playlists.clear();
+    _playlists.addAll(res);
+    _loading = false;
+    notifyListeners();
   }
 
   void createPlaylist(Playlist playlist) async {
     assert(playlist.id == null);
-    //TODO
-    throw UnimplementedError();
+    _loading = true;
+    var res = await service.create(playlist);
+    _playlists.add(res);
+    _loading = false;
+    notifyListeners();
   }
 
   void addPlaylist(Playlist playlist) {
@@ -30,9 +47,11 @@ class PlaylistsProvider with ChangeNotifier {
 
   void deletePlaylist(Playlist playlist) async {
     assert(playlist.id != null);
+    _loading = true;
+    await service.destroy(playlist);
+    _loading = false;
 
-    //TODO
-    throw UnimplementedError();
+    _playlists.remove(playlist);
   }
 
   void removePlaylist(Playlist playlist) {
@@ -43,8 +62,17 @@ class PlaylistsProvider with ChangeNotifier {
 
   void updatePlaylist(Playlist playlist) async {
     assert(playlist.id != null);
+    _loading = true;
 
-    //TODO
-    throw UnimplementedError();
+    var res = await service.update(playlist);
+
+    int index = _playlists.indexOf(playlist);
+
+    if (index == -1) return;
+
+    _playlists[index] = res;
+    _loading = false;
+
+    notifyListeners();
   }
 }
