c9edb3465b22ddedef08117aa30b2fa4a22242da
M S <mo.sa963@hotmail.com>
2023-06-30 22:52:48 +0300

    Add AudioPlayButton widget

---
+++ lib/widgets/audio_play_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+
+class AudioPlayButton extends StatelessWidget {
+  const AudioPlayButton({super.key, required this.audio, this.size = 50});
+  final double size;
+  final AudioPlayer audio;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<PlayerState>(
+      stream: audio.playerStateStream,
+      builder: (context, snapshot) => IconButton.outlined(
+          style: const ButtonStyle(
+              padding: MaterialStatePropertyAll(EdgeInsets.all(3))),
+          onPressed: onPressed,
+          icon: Icon(
+            snapshot.data?.playing ?? false ? Icons.pause : Icons.play_arrow,
+            size: size,
+          ),
+          padding: const EdgeInsets.all(0)),
+    );
+  }
+
+  void onPressed() {
+    audio.playing ? audio.pause() : audio.play();
+  }
+}
