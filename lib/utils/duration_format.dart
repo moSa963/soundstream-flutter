de41c0b8605b36c41506405f32698744ac3f0c7d
M S <mo.sa963@hotmail.com>
2023-07-01 17:54:59 +0300

    Add DurationFormat

---
+++ lib/utils/duration_format.dart
@@ -0,0 +1,19 @@
+class DurationFormat {
+  // return duration as hours:minutes:seconds string
+  static String toHms(Duration? duration) {
+    if (duration == null) return "0:00";
+
+    int seconds = duration.inSeconds;
+    int hours = (seconds / 3600).floor();
+    int minutes = ((seconds -= (hours * 3600)) / 60).floor();
+    int rseconds = (seconds - (minutes * 60)).floor();
+
+    String res = "";
+
+    if (hours > 0) {
+      res += "$hours:";
+    }
+
+    return "$res$minutes:$rseconds";
+  }
+}
