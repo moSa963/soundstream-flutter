a62471eadaa41f544b7f8bfc913b64dd3a8ce17f
M S <mo.sa963@hotmail.com>
2023-07-02 19:26:58 +0300

    Add validator

---
+++ lib/utils/validator.dart
@@ -0,0 +1,36 @@
+class Validator {
+  /// validate [value]
+  /// 
+  /// Returns an error message or null if there is no error.
+  static String? validate(String? value,
+      {bool required = true,
+      int? max,
+      int? min,
+      String? regex,
+      String? equal}) {
+    if (value == null || value.isEmpty) {
+      if (required) {
+        return "This field is required";
+      }
+      return null;
+    }
+
+    if (max != null && value.length > max) {
+      return "The maximum length is 255";
+    }
+
+    if (min != null && value.length < min) {
+      return "The minimum length is 255";
+    }
+
+    if (regex != null && !RegExp(regex).hasMatch(value)) {
+      return "Incorrect format";
+    }
+
+    if (equal != null && value != equal) {
+      return "This is not a valid value";
+    }
+
+    return null;
+  }
+}
