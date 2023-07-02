74029fcbe9ae6f8654a092d2d15299c9ad2a74af
M S <mo.sa963@hotmail.com>
2023-07-02 15:29:18 +0300

    Add text scale button

---
+++ lib/widgets/text_scale_button.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:soundstream_flutter/widgets/scale_gesture_detector.dart';
+
+class TextScaleButton extends StatelessWidget {
+  const TextScaleButton(this.text, {super.key, this.onTap});
+  final String text;
+  final void Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaleGestureDetector(
+      onTap: onTap,
+      child: Text(text,
+          style: TextStyle(color: Theme.of(context).colorScheme.primary)),
+    );
+  }
+}
