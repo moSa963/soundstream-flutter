082b8bb0b755eaa385a39d12a014553dcc40fd2c
M S <mo.sa963@hotmail.com>
2023-07-05 23:01:03 +0300

    Add loadData method in show playlist page to load its tracks

--- lib/pages/show_playlist_page/show_playlist_page.dart
+++ lib/pages/show_playlist_page/show_playlist_page.dart
@@ -1,21 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:soundstream_flutter/models/playlist.dart';
 import 'package:soundstream_flutter/models/track.dart';
+import 'package:soundstream_flutter/pages/player_page/player_page.dart';
 import 'package:soundstream_flutter/pages/show_playlist_page/show_playlist_banner.dart';
+import 'package:soundstream_flutter/providers/audio_queue_provider.dart';
+import 'package:soundstream_flutter/services/playlist_service.dart';
+import 'package:soundstream_flutter/services/track_service.dart';
 import 'package:soundstream_flutter/widgets/track_item.dart';
 
 class ShowPlaylistPage extends StatefulWidget {
   const ShowPlaylistPage({super.key, required this.playlist});
 
   final Playlist playlist;
+  final _service = const PlaylistService();
+  final _trackService = const TrackService();
 
   @override
   State<ShowPlaylistPage> createState() => _ShowPlaylistPageState();
 }
 
 class _ShowPlaylistPageState extends State<ShowPlaylistPage> {
-  final List<Track> tracks = [];
+  List<Track>? _tracks;
+  Playlist? _playlist;
 
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,10 +36,33 @@ class _ShowPlaylistPageState extends State<ShowPlaylistPage> {
       appBar: AppBar(),
       body: ListView(
         children: [
-          ShowPlaylistBanner(playlist: widget.playlist),
-          TrackItem(track: Track(title: "TITLE")),
+          ShowPlaylistBanner(playlist: _playlist ?? widget.playlist),
+          for (var track in _tracks ?? []) TrackItem(track: track, onTap: () => handleTap(track)),
         ],
       ),
     );
+  }
+
+  void loadData() async {
+    Playlist p = await widget._service.get(widget.playlist.id ?? 0);
+
+    setState(() {
+      _playlist = p;
+    });
+
+    var tracks = await widget._trackService.list(p);
+
+    setState(() {
+      _tracks = tracks;
+    });
+  }
+
+  void handleTap(Track track) {
+    context.read<AudioQueueProvider>()
+    .setList(_tracks ?? [], index: _tracks?.indexOf(track));
+
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+      return const PlayerPage();
+    },));
   }
 }
