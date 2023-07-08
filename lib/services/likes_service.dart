2a57bcd118b9580c0567933e92030e8ece3f3ff3
M S <mo.sa963@hotmail.com>
2023-07-08 23:38:26 +0300

    Add likes service

---
+++ lib/services/likes_service.dart
@@ -0,0 +1,31 @@
+
+import 'package:soundstream_flutter/models/playlist.dart';
+import 'package:soundstream_flutter/models/track.dart';
+import 'package:soundstream_flutter/services/api_service.dart';
+
+class LikesService {
+  const LikesService();
+  final api = const ApiService();
+
+  Future<List<Track>> likedTracks() async {
+    final js = await api.get("likes");
+    return js["data"].map((v) => Track.fromJson(v)).toList();
+  }
+
+  Future<void> likeTrack(Track track) async {
+    await api.post("likes/tracks/${track.id}", null);
+  }
+
+  Future<void> unlikeTrack(Track track) async {
+    await api.delete("likes/tracks/${track.id}");
+  }
+
+  Future<void> likePlaylist(Playlist playlist) async {
+    await api.post("likes/playlists/${playlist.id}", null);
+  }
+
+  Future<void> unlikePlaylist(Playlist playlist) async {
+    await api.delete("likes/playlists/${playlist.id}");
+  }
+
+}
