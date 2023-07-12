30b42be5c44360720ece46ec4e042e52f60a592e
M S <mo.sa963@hotmail.com>
2023-07-12 21:30:44 +0300

    Update PlaylistsProvider to extend Provider and use withLoading method

--- lib/providers/playlists_provider.dart
+++ lib/providers/playlists_provider.dart
@@ -1,42 +1,36 @@
-import 'package:flutter/material.dart';
 import 'package:soundstream_flutter/models/playlist.dart';
+import 'package:soundstream_flutter/providers/provider.dart';
 import 'package:soundstream_flutter/services/playlist_service.dart';
 
-class PlaylistsProvider with ChangeNotifier {
+class PlaylistsProvider extends Provider {
   final List<Playlist> _playlists;
   List<Playlist> get playlists => _playlists;
 
-  bool _loading;
-  bool get loading => _loading;
-
   final PlaylistService service;
 
-  PlaylistsProvider({required this.service})
-      : _playlists = [],
-        _loading = false {
-          loadPlaylists();
-        }
-
-  void loadPlaylists() async {
-    _loading = true;
-    var res = await service.list();
-    _playlists.clear();
-    _playlists.addAll(res);
-    _loading = false;
-    notifyListeners();
+  PlaylistsProvider({required this.service}) : _playlists = [] {
+    loadPlaylists();
   }
 
-  void createPlaylist(Playlist playlist) async {
-    assert(playlist.id == null);
-    _loading = true;
-    var res = await service.create(playlist);
-    _playlists.add(res);
-    _loading = false;
-    notifyListeners();
+  Future<void> loadPlaylists() async {
+    await withLoading(() async {
+      var res = await service.list();
+      _playlists.clear();
+      _playlists.addAll(res);
+    });
+  }
+
+  Future<void> createPlaylist(Playlist playlist) async {
+    assert(playlist.id == -1);
+
+    await withLoading(() async {
+      var res = await service.create(playlist);
+      _playlists.add(res);
+    });
   }
 
   void addPlaylist(Playlist playlist) {
-    assert(playlist.id != null);
+    assert(playlist.id != -1);
     if (playlists.where((element) => element.id == playlist.id).isNotEmpty) {
       return;
     }
@@ -45,34 +39,34 @@ class PlaylistsProvider with ChangeNotifier {
     notifyListeners();
   }
 
-  void deletePlaylist(Playlist playlist) async {
-    assert(playlist.id != null);
-    _loading = true;
-    await service.destroy(playlist);
-    _loading = false;
+  Future<void> deletePlaylist(Playlist playlist) async {
+    assert(playlist.id != -1);
 
-    _playlists.remove(playlist);
+    await withLoading(() async {
+      await service.destroy(playlist);
+      _playlists.removeWhere((element) => element.id == playlist.id);
+    });
   }
 
   void removePlaylist(Playlist playlist) {
-    assert(playlist.id != null);
+    assert(playlist.id != -1);
     playlists.removeWhere((element) => element.id == playlist.id);
     notifyListeners();
   }
 
-  void updatePlaylist(Playlist playlist) async {
-    assert(playlist.id != null);
-    _loading = true;
+  Future<void> updatePlaylist(Playlist playlist) async {
+    await withLoading(() async {
+      late Playlist res;
 
-    var res = await service.update(playlist);
+      res = await service.update(playlist);
 
-    int index = _playlists.indexOf(playlist);
+      int index = _playlists.indexWhere(
+        (element) => element.id == playlist.id,
+      );
 
-    if (index == -1) return;
+      if (index == -1) return;
 
-    _playlists[index] = res;
-    _loading = false;
-
-    notifyListeners();
+      _playlists[index] = res;
+    });
   }
 }
