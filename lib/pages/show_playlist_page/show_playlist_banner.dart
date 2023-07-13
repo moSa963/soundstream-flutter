5a7ec39e2dd9c3e8ba73eb4bc3235eebab6c8624
M S <mo.sa963@hotmail.com>
2023-07-13 22:07:16 +0300

    Add _onUpdate method to show UpdatePlaylistSheet

--- lib/pages/show_playlist_page/show_playlist_banner.dart
+++ lib/pages/show_playlist_page/show_playlist_banner.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:soundstream_flutter/models/playlist.dart';
 import 'package:soundstream_flutter/widgets/page_banner.dart';
+import 'package:soundstream_flutter/widgets/bottom_sheet/update_playlist_sheet.dart';
 
 class ShowPlaylistBanner extends StatelessWidget {
   const ShowPlaylistBanner({super.key, required this.playlist});
@@ -9,13 +10,18 @@ class ShowPlaylistBanner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PageBanner(
-      title: playlist.title,
-      subtitle: playlist.description,
+      title: playlist.title ?? "",
+      subtitle: playlist.description ?? "",
       image: Image.network( playlist.imgUri.toString(), fit: BoxFit.contain, ),
-      actions: [
-        IconButton(onPressed: () {}, icon: const Icon(Icons.update)),
-        IconButton(onPressed: () {}, icon: const Icon(Icons.remove)),
+      leading: [
+          IconButton(onPressed: () => _onUpdate(context), icon: const Icon(Icons.edit)),
       ],
     );
+  }
+
+  void _onUpdate(BuildContext context) {
+    showModalBottomSheet(context: context, builder: (context) {
+      return UpdatePlaylistSheet(playlist: playlist);
+    });
   }
 }
