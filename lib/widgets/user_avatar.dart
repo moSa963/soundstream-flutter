c7c25be64763c6fae8e2cf25ea276e54a8a8ee6b
M S <mo.sa963@hotmail.com>
2023-07-15 19:08:05 +0300

    Add image and maxWidth prop to UserAvatar

--- lib/widgets/user_avatar.dart
+++ lib/widgets/user_avatar.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:soundstream_flutter/models/user.dart';
 import 'package:soundstream_flutter/widgets/button/scale_gesture_detector.dart';
 
 class UserAvatar extends StatelessWidget {
-  const UserAvatar({super.key});
+  const UserAvatar({super.key, this.maxWidth, required this.user});
+  final double? maxWidth;
+  final User? user;
 
   @override
   Widget build(BuildContext context) {
     return ScaleGestureDetector(
         child: Container(
+      constraints: BoxConstraints(maxWidth: maxWidth ?? double.infinity),
       padding: const EdgeInsets.all(5),
-      child: const CircleAvatar(),
+      child: CircleAvatar(
+        backgroundImage: user?.imgUri != null ? NetworkImage(user?.imgUri.toString() ?? "") : null, 
+      ),
     ));
   }
 }
