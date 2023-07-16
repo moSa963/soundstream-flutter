d838891e267ff44032a569369a8de6a6d6aa2ab4
M S <mo.sa963@hotmail.com>
2023-07-16 17:11:49 +0300

    Add action to ShowPlaylistBanner

--- lib/pages/show_playlist_page/show_playlist_banner.dart
+++ lib/pages/show_playlist_page/show_playlist_banner.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:soundstream_flutter/models/playlist.dart';
+import 'package:soundstream_flutter/widgets/button/scale_gesture_detector.dart';
 import 'package:soundstream_flutter/widgets/page_banner.dart';
 import 'package:soundstream_flutter/widgets/bottom_sheet/update_playlist_sheet.dart';
+import 'package:soundstream_flutter/widgets/user_avatar.dart';
 
 class ShowPlaylistBanner extends StatelessWidget {
   const ShowPlaylistBanner({super.key, required this.playlist});
@@ -12,16 +14,51 @@ class ShowPlaylistBanner extends StatelessWidget {
     return PageBanner(
       title: playlist.title ?? "",
       subtitle: playlist.description ?? "",
-      image: Image.network( playlist.imgUri.toString(), fit: BoxFit.contain, ),
+      image: Image.network(
+        playlist.imgUri.toString(),
+        fit: BoxFit.contain,
+      ),
       leading: [
-          IconButton(onPressed: () => _onUpdate(context), icon: const Icon(Icons.edit)),
+        Text(
+          playlist.album ?? false ? "Album" : "Playlist",
+          style: const TextStyle(color: Colors.grey),
+        ),
+        const Spacer(),
+        IconButton(
+            onPressed: () => _onUpdate(context), icon: const Icon(Icons.edit)),
+      ],
+      actions: [_action(context)],
+    );
+  }
+
+  Widget _action(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ScaleGestureDetector(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              UserAvatar(user: playlist.user, maxWidth: 35),
+              Text(
+                "@${playlist.user?.username}",
+                style: Theme.of(context).textTheme.titleSmall,
+              )
+            ],
+          ),
+        ),
+        const Text(" . "),
+        Text(
+            "${playlist.private ?? false ? 'Public' : 'Private'} ${playlist.tracksCount} track"),
       ],
     );
   }
 
   void _onUpdate(BuildContext context) {
-    showModalBottomSheet(context: context, builder: (context) {
-      return UpdatePlaylistSheet(playlist: playlist);
-    });
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return UpdatePlaylistSheet(playlist: playlist);
+        });
   }
 }
