f9cf332eb0fd3877b28be39abe842dcc6ba8d563
M S <mo.sa963@hotmail.com>
2023-07-23 15:57:24 +0300

    Add user list item

---
+++ lib/widgets/list_item/user_item.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:soundstream_flutter/models/user.dart';
+import 'package:soundstream_flutter/widgets/list_item/list_item.dart';
+
+class UserItem extends StatelessWidget {
+  final User user;
+
+  const UserItem({super.key, required this.user});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListItem(
+      onTap: () => _openShowUserPage(context),
+      leading: AspectRatio(aspectRatio: 1, child: Hero(tag: user.username, child: Image.network(user.imgUri.toString(), fit: BoxFit.cover,))),
+      title: user.name,
+      subtitle: "@${user.username}"
+    );
+  }
+
+  void _openShowUserPage(BuildContext context) {
+    //TODO
+  }
+}
