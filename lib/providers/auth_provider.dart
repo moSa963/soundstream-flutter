de49a12e6eb52426bb7b3f5e6591ac406ab0dc5f
M S <mo.sa963@hotmail.com>
2023-07-23 18:23:54 +0300

    Add register, login and logout methods to AuthProvider

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -15,4 +15,34 @@ class AuthProvider extends Provider {
       auth = await service.getUser();
     });
   }
+
+  Future<void> register(
+      {required String name,
+      required String email,
+      required String username,
+      required String password,
+      required String passwordConfirmation}) async {
+    withLoading(() async {
+      auth = await service.register(
+          name: name,
+          email: email,
+          username: username,
+          password: password,
+          passwordConfirmation: passwordConfirmation);
+    });
+  }
+
+  Future<void> login(
+      {required String username, required String password}) async {
+    withLoading(() async {
+      auth = await service.login(username: username, password: password);
+    });
+  }
+
+  Future<void> logout() async {
+    withLoading(() async {
+      await service.logout();
+      auth = null;
+    });
+  }
 }
