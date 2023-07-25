ac91797541851b98bb222c3f507c45311f5718e6
M S <mo.sa963@hotmail.com>
2023-07-25 20:20:49 +0300

    Refactor register page and add handleError method

--- lib/pages/register_page/register_page.dart
+++ lib/pages/register_page/register_page.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:soundstream_flutter/pages/register_page/login_form.dart';
 import 'package:soundstream_flutter/pages/register_page/signup_form.dart';
+import 'package:soundstream_flutter/providers/auth_provider.dart';
+import 'package:soundstream_flutter/widgets/loading_box.dart';
 import 'package:soundstream_flutter/widgets/logo_banner.dart';
 import 'package:soundstream_flutter/widgets/button/text_scale_button.dart';
 
 class RegisterPage extends StatefulWidget {
-  RegisterPage({super.key});
-  final List<Widget> forms = [
-    const LoginForm(),
-    const SignupForm(),
-  ];
+  const RegisterPage({super.key});
 
   @override
   State<RegisterPage> createState() => _RegisterPageState();
@@ -18,36 +17,69 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   int _index = 0;
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         padding: const EdgeInsets.symmetric(horizontal: 25),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            LogoBanner(title: "SoundStream", direction: _index == 1 ? Axis.horizontal : Axis.vertical, maxHeight: 100),
-            widget.forms[_index],
-            _navText(),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 50),
+              alignment: Alignment.center,
+              child: const LogoBanner(title: "SoundStream", maxHeight: 100),
+            ),
+            Expanded(
+              child: Center(
+                child: SingleChildScrollView(
+                  child: _getForm(),
+                ),
+              ),
+            ),
+            _navText()
           ],
         ),
       ),
     );
   }
 
+  Widget _getForm() {
+    if (context.watch<AuthProvider>().loading) {
+      return const LoadingBox();
+    }
+
+    switch (_index) {
+      case 1:
+        return SignupForm(onError: handleError);
+      default:
+        return LoginForm(onError: handleError);
+    }
+  }
+
   Widget _navText() {
     return Container(
+      padding: const EdgeInsets.symmetric(vertical: 50),
       alignment: Alignment.topLeft,
       child: Wrap(
         children: [
           Text("you${_index == 0 ? " dont " : " "}have an account? "),
-          TextScaleButton(_index == 0 ? " sign up" : " login", onTap: () {
-            setState(() {
-              _index = _index == 1 ? 0 : 1;
-            });
-          },),
+          TextScaleButton(
+            _index == 0 ? " sign up" : " login",
+            onTap: () {
+              setState(() {
+                _index = _index == 1 ? 0 : 1;
+              });
+            },
+          ),
         ],
+      ),
+    );
+  }
+
+  void handleError(String error) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(error),
       ),
     );
   }
