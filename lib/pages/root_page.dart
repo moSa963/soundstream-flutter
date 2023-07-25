bfee5269d5864e8b795637e91fcc58b0ceda7376
M S <mo.sa963@hotmail.com>
2023-07-25 20:22:17 +0300

    Remove LoadingPage and let RegisterPage handle the loading

--- lib/pages/root_page.dart
+++ lib/pages/root_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:soundstream_flutter/pages/home_page/home_nav_page.dart';
-import 'package:soundstream_flutter/pages/loading_page.dart';
 import 'package:soundstream_flutter/pages/register_page/register_page.dart';
 import 'package:soundstream_flutter/providers/auth_provider.dart';
 import 'package:soundstream_flutter/providers/theme_provider.dart';
@@ -18,12 +17,8 @@ class RootPage extends StatelessWidget {
       themeMode: context.watch<ThemeProvider>().themeMode,
       home: Consumer<AuthProvider>(
         builder: (context, value, child) {
-          if (value.loading || child == null) {
-            return const LoadingPage();
-          }
-
-          if (value.auth == null) {
-            return RegisterPage();
+          if (value.loading || value.auth == null || child == null) {
+            return const RegisterPage();
           }
 
           return child;
