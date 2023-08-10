5215faa20c61a2fc881043231fb0c6b4cf8e59c0
M S <mo.sa963@hotmail.com>
2023-08-10 21:24:07 +0300

    Add bottom sheet to choose between creating playlist or album in LibrarPageAppbar

--- lib/pages/library_page/library_page_appbar.dart
+++ lib/pages/library_page/library_page_appbar.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:soundstream_flutter/pages/create_album_page.dart';
 import 'package:soundstream_flutter/pages/create_playlist_page.dart';
 import 'package:soundstream_flutter/providers/auth_provider.dart';
 import 'package:soundstream_flutter/widgets/user_avatar.dart';
-
-
 
 class LibraryPageAppbar extends StatelessWidget implements PreferredSizeWidget {
   const LibraryPageAppbar({super.key});
@@ -18,20 +17,41 @@ class LibraryPageAppbar extends StatelessWidget implements PreferredSizeWidget {
       leading: UserAvatar(user: context.watch<AuthProvider>().auth),
       actions: [
         IconButton(
-          onPressed: () => openPage(context, const CreatePlaylistPage()), 
-          icon: const Icon(Icons.add)
-        )
+            onPressed: () => _onCreate(context), icon: const Icon(Icons.add))
       ],
     );
   }
 
-
   void openPage(BuildContext context, Widget page) {
-      Navigator.of(context).push(MaterialPageRoute( builder: (context) {
+    Navigator.pop(context);
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) {
         return page;
-      },));
+      },
+    ));
   }
-  
+
+  void _onCreate(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                    onPressed: () =>
+                        openPage(context, const CreatePlaylistPage()),
+                    child: const Text("Create Playlist")),
+                TextButton(
+                    onPressed: () => openPage(context, const CreateAlbumPage()),
+                    child: const Text("Create Album")),
+              ],
+            ),
+          );
+        });
+  }
+
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
