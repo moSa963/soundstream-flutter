5fa9214844ec3aae34b0e93582be66c221e6436f
M S <mo.sa963@hotmail.com>
2023-08-13 18:55:37 +0300

    Add padLeft to minutes and seconds in DurationFormat

--- lib/utils/duration_format.dart
+++ lib/utils/duration_format.dart
@@ -14,6 +14,6 @@ class DurationFormat {
       res += "$hours:";
     }
 
-    return "$res$minutes:$rseconds";
+    return "$res${minutes.toString().padLeft(2, "0")}:${rseconds.toString().padLeft(2, "0")}";
   }
 }
