1ccae0732509c1a18769e2dc22d3b1725924b9b2
M S <mo.sa963@hotmail.com>
2023-11-18 18:21:30 +0300

    Add onTap callback to UserAvatar widget

--- lib/widgets/user_avatar.dart
+++ lib/widgets/user_avatar.dart
@@ -3,13 +3,15 @@ import 'package:soundstream_flutter/models/user.dart';
 import 'package:soundstream_flutter/widgets/button/scale_gesture_detector.dart';
 
 class UserAvatar extends StatelessWidget {
-  const UserAvatar({super.key, this.maxWidth, required this.user});
+  const UserAvatar({super.key, this.maxWidth, required this.user, this.onTap});
   final double? maxWidth;
   final User? user;
+  final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
     return ScaleGestureDetector(
+      onTap: onTap,
         child: Container(
       constraints: BoxConstraints(maxWidth: maxWidth ?? double.infinity),
       padding: const EdgeInsets.all(5),
