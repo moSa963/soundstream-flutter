6fb9b328bc7b74cfa33079586872024ea6fe105a
M S <mo.sa963@hotmail.com>
2023-11-29 21:24:44 +0300

    Add ProfilePage

--- lib/pages/profile_page/profile_page.dart
+++ lib/pages/profile_page/profile_page.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:soundstream_flutter/models/user.dart';
 import 'package:soundstream_flutter/pages/page_layout.dart';
+import 'package:soundstream_flutter/pages/user_page/user_page_header.dart';
+import 'package:soundstream_flutter/providers/auth_provider.dart';
+import 'package:soundstream_flutter/widgets/button/update_profile_image_button.dart';
+import 'package:soundstream_flutter/widgets/text_title.dart';
+import 'package:soundstream_flutter/widgets/theme_switch.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -7,8 +14,31 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PageLayout(
-      appBar: AppBar(),
-      body: const Placeholder(),
-    );
+        body: Column(
+      children: [
+        UserPageHeader(user: context.watch<AuthProvider>().auth ?? User()),
+        Expanded(
+          child: ListView(
+            shrinkWrap: true,
+            padding: const EdgeInsets.all(10),
+            children: const [
+              TextTitle("Profile"),
+              UpdateProfileImageButton(),
+              SizedBox(
+                height: 20,
+              ),
+              TextTitle("Theme"),
+              Wrap(
+                crossAxisAlignment: WrapCrossAlignment.center,
+                children: [
+                  Text("Dark theme: "),
+                  ThemeSwitch(),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    ));
   }
 }
