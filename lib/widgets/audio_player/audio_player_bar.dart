151393df067fd2dd7326805483487af6f2848a0f
M S <mo.sa963@hotmail.com>
2023-12-10 21:29:09 +0300

    Add MovableBox to AudioPlayerBar and empty the audio queue when user drags down

--- lib/widgets/audio_player/audio_player_bar.dart
+++ lib/widgets/audio_player/audio_player_bar.dart
@@ -6,6 +6,7 @@ import 'package:soundstream_flutter/providers/audio_queue_provider.dart';
 import 'package:soundstream_flutter/widgets/audio_player/audio_play_button.dart';
 import 'package:soundstream_flutter/widgets/audio_player/audio_progress_bar.dart';
 import 'package:soundstream_flutter/widgets/list_item/track_item.dart';
+import 'package:soundstream_flutter/widgets/movable_box.dart';
 
 class AudioPlayerBar extends StatelessWidget {
   const AudioPlayerBar({super.key});
@@ -15,43 +16,46 @@ class AudioPlayerBar extends StatelessWidget {
   Widget build(BuildContext context) {
     var provider = context.watch<AudioQueueProvider>();
 
-    return Container(
-      constraints: const BoxConstraints(maxWidth: 800),
-      margin: const EdgeInsets.all(10),
-      clipBehavior: Clip.antiAlias,
-      decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.background,
-          borderRadius: const BorderRadius.all(Radius.circular(5)),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withAlpha(100),
-              blurRadius: 10,
-            )
-          ]),
+    return MovableBox(
+      onPanEnd: (e) => onPanEnd(context, e),
       child: Container(
-          color: Theme.of(context).colorScheme.secondary.withAlpha(50),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(5),
-                child: Row(
-                  children: [
-                    Expanded(
-                        child: TrackItem(
-                            onTap: () => _openPlayerPage(context),
-                            track: provider.track ?? Track(),
-                            withHero: false)),
-                    const AudioPlayButton(),
-                  ],
-                ),
-              ),
-              const AudioProgressBar(
-                noLabels: true,
-                noSeek: true,
-                trackHeight: 3,
+        constraints: const BoxConstraints(maxWidth: 800),
+        margin: const EdgeInsets.all(10),
+        clipBehavior: Clip.antiAlias,
+        decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.background,
+            borderRadius: const BorderRadius.all(Radius.circular(5)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withAlpha(100),
+                blurRadius: 10,
               )
-            ],
-          )),
+            ]),
+        child: Container(
+            color: Theme.of(context).colorScheme.secondary.withAlpha(50),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: TrackItem(
+                              onTap: () => _openPlayerPage(context),
+                              track: provider.track ?? Track(),
+                              withHero: false)),
+                      const AudioPlayButton(),
+                    ],
+                  ),
+                ),
+                const AudioProgressBar(
+                  noLabels: true,
+                  noSeek: true,
+                  trackHeight: 3,
+                )
+              ],
+            )),
+      ),
     );
   }
 
@@ -61,5 +65,11 @@ class AudioPlayerBar extends StatelessWidget {
         return const PlayerPage();
       },
     ));
+  }
+
+  void onPanEnd(BuildContext context, DragEndDetails e) {
+    if (e.velocity.pixelsPerSecond.dy > 1000) {
+      context.read<AudioQueueProvider>().setList([]);
+    }
   }
 }
