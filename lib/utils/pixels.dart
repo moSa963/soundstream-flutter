bce105d747bab60b708964c7ec81938c6378f5ac
moSa963 <mo.sa963@hotmail.com>
2023-12-23 14:02:23 +0300

    Refactor findDominantColor function so it combine the first six dominant colors

--- lib/utils/pixels.dart
+++ lib/utils/pixels.dart
@@ -1,8 +1,10 @@
 import 'dart:isolate';
 import 'dart:math';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import "package:image/image.dart" as img;
+import 'package:soundstream_flutter/utils/ordered_list.dart';
 
 class Pixels {
   /// Returns the dominat color of an image
@@ -12,50 +14,74 @@ class Pixels {
   /// [gap] is how much pixel to jump each iteration
   static Future<Color?> findDominantColor(img.Image image,
       {int sensitivity = 30, int gap = 16}) async {
-    img.Pixel? pixel = await Isolate.run<img.Pixel?>(() {
-      final map = {};
-      int max = 0;
-      img.Pixel? color;
+    final colors =
+        await Isolate.run(() => _getColorList(image, gap, sensitivity));
 
-      for (int i = 0; i < image.width; i += gap) {
-        for (int j = 0; j < image.height; j += gap) {
-          final pixel = image.getPixel(i, j);
+    if (colors.isEmpty) return null;
 
-          if (pixel.a < 150) {
-            continue;
-          }
+    return mix(colors
+        .map((e) => e["pixel"] as img.Pixel)
+        .toList()
+        .sublist(0, min(6, colors.length)));
+  }
 
-          bool found = false;
+  static List<Map<String, dynamic>> _getColorList(
+      img.Image image, int gap, int sensitivity) {
+    OrderedList<Map<String, dynamic>> colors =
+        OrderedList(compare: (v1, v2) => v1["count"] <= v2["count"]);
 
-          for (final value in map.values) {
-            if (distance(value["pixel"], pixel) < sensitivity) {
-              value["count"]++;
-              if (value["count"]! > max) {
-                max = value["count"]!;
-                color = pixel;
-              }
-              found = true;
-            }
-          }
+    for (int i = 0; i < image.width; i += gap) {
+      for (int j = 0; j < image.height; j += gap) {
+        final pixel = image.getPixel(i, j);
 
-          if (!found) {
-            String key = "${pixel.r} ${pixel.g} ${pixel.b}";
-            map[key] = {"pixel": pixel, "count": 0};
+        if (pixel.a < 150) {
+          continue;
+        }
+
+        bool found = false;
+
+        for (int i = 0; i < colors.length; ++i) {
+          if (distance(colors[i]["pixel"], pixel) < sensitivity) {
+            final item = colors[i];
+
+            colors.remove(item);
+
+            item["count"]++;
+
+            colors.add(item);
+
+            found = true;
+            break;
           }
         }
+
+        if (!found) {
+          colors.add({"pixel": pixel, "count": 0});
+        }
       }
+    }
 
-      return color;
-    });
-
-    if (pixel == null) return null;
-
-    return Color.fromRGBO(pixel.r.toInt(), pixel.g.toInt(), pixel.b.toInt(), 1);
+    return colors.toList();
   }
 
   ///Returns the distance between two colors
   static num distance(img.Pixel p1, img.Pixel p2) {
     return sqrt(
         pow(p1.r - p2.r, 2) + pow(p1.g - p2.g, 2) + pow(p1.b - p2.b, 2));
+  }
+
+  static Color mix(List<img.Pixel> colors) {
+    num r = 0;
+    num g = 0;
+    num b = 0;
+
+    for (int i = 0; i < colors.length; ++i) {
+      r += colors[i].r;
+      g += colors[i].g;
+      b += colors[i].b;
+    }
+
+    return Color.fromRGBO(
+        r ~/ colors.length, g ~/ colors.length, b ~/ colors.length, 1);
   }
 }
