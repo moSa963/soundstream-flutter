138e0370c60eafe3bb7046716964d042dca381ed
moSa963 <mo.sa963@hotmail.com>
2024-01-01 19:55:47 +0300

    Add just audio background initializer to main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:just_audio_background/just_audio_background.dart';
 import 'package:provider/provider.dart';
 import 'package:soundstream_flutter/pages/root_page.dart';
 import 'package:soundstream_flutter/providers/audio_queue_provider/audio_queue_provider.dart';
@@ -9,14 +12,22 @@ import 'package:soundstream_flutter/providers/upload_provider.dart';
 import 'package:soundstream_flutter/services/auth_service.dart';
 import 'package:soundstream_flutter/services/playlist_service.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  if (Platform.isAndroid || Platform.isIOS) {
+    await JustAudioBackground.init(
+      androidNotificationChannelId: 'com.ryanheise.soundstream.channel.audio',
+      androidNotificationChannelName: 'Audio playback',
+    );
+  }
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => ThemeProvider()),
